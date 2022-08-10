cbac3f03467054492f2082604da97316fea79cf6
latz09 <85132328+latz09@users.noreply.github.com>
2022-08-10 15:24:28 -0500

    added house icon to nav

--- components/layout/Navbar.js
+++ components/layout/Navbar.js
@@ -1,3 +1,5 @@
+import { BsFillHouseFill } from 'react-icons/bs';
+
 import Link from 'next/link';
 import NavLink from '../utils/NavLink';
 
@@ -5,9 +7,12 @@ const Navbar = () => {
 	return (
 		<div className='bg-gray-50'>
 			<div className=' px-5 py-2 flex justify-between items-center max-w-7xl mx-auto'>
-				<div className='font-logo tracking-wider text-xl md:text-3xl text-pizzaRed-500 hover:opacity-80 cursor-pointer transition duration-700'>
+				<div className='font-logo tracking-wider text-xl md:text-3xl text-pizzaRed-500 hover:opacity-70 cursor-pointer transition duration-700'>
 					<Link href={'/'}>
+						<div className="flex space-x-2 items-center">
+							<BsFillHouseFill />
 						<span>The Pizza Shop</span>
+						</div>
 					</Link>
 				</div>
 				<div>
