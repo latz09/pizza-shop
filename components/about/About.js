008dadc9287f0824a7cac9e702a806971348d850
latz09 <85132328+latz09@users.noreply.github.com>
2022-08-17 15:56:15 -0500

    added red to about

--- components/about/About.js
+++ components/about/About.js
@@ -4,7 +4,10 @@ const About = () => {
 	return (
 		<>
 			<div className='max-w-7xl mx-auto py-32 text-4xl md:text-5xl text-gray-700 font-logo tracking-widest '>
-				<p className="mx-3 text-center">Proudly Serving West Saint Paul Since 19##</p>
+				<p className='mx-3 text-center'>
+					Proudly Serving the City of{' '}
+					<span className='text-pizzaRed-500'> West Saint Paul</span> Since 19##
+				</p>
 			</div>
 		</>
 	);
