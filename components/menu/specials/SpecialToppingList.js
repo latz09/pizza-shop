bb4a0442b6dcaf68e7f9b660bf6c06b50748a222
latz09 <85132328+latz09@users.noreply.github.com>
2022-09-05 08:41:35 -0500

    removed bordedr

--- components/menu/specials/SpecialToppingList.js
+++ components/menu/specials/SpecialToppingList.js
@@ -2,7 +2,7 @@ import ToppingItem from "../buildYourOwn/ToppingItem";
 
 const SpecialToppingList = ({toppings}) => {
 	return (
-		<ul className=' text-pizzaRed-300 flex flex-wrap justify-center items-center text-xs gap-2 sm:gap-4 sm:text-lg py-4 px-5 border border-black'>
+		<ul className=' text-pizzaRed-300 flex flex-wrap justify-center items-center text-xs gap-2 sm:gap-4 sm:text-lg py-4 px-5'>
 			{toppings.map((topping) => (
 				<ToppingItem key={topping} item={topping} />
 			))}
