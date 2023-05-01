372defc2d117700cfce8bbf0a356316e43c9fc4a
latz09 <85132328+latz09@users.noreply.github.com>
2023-05-01 15:41:58 -0500

    d

--- components/layout/Navbar.js
+++ components/layout/Navbar.js
@@ -6,7 +6,7 @@ import NavLink from '../utils/NavLink';
 const Navbar = () => {
 	return (
 		<div className='border-b border-secondary/50 shadow-md bg-light/80'>
-			<div className=' py-3 px-2 xl:px-0 flex justify-between items-center max-w-5xl mx-auto '>
+			<div className=' py-3 lg:py-4 px-2 xl:px-0 flex justify-between items-center max-w-5xl mx-auto '>
 				<div className='tracking-wider text-lg md:text-xl  hover:opacity-70 cursor-pointer transition duration-700'>
 					<Link href={'/'}>
 						<div className='flex space-x-2 items-center text-primary'>
