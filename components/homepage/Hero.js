9ef63bc47ef13fc748d1d4eb3898e614a301aaf5
latz09 <85132328+latz09@users.noreply.github.com>
2023-05-01 14:37:40 -0500

    an

--- components/homepage/Hero.js
+++ components/homepage/Hero.js
@@ -23,21 +23,30 @@ const Hero = () => {
 					transition={{ duration: 1.5, delay: .9 }}
 				className='grid gap-6'>
 					<h1 className='text-5xl md:text-7xl  scale-y-125'>The Pizza Shop</h1>
-					<div className='grid gap-2 text-xl text-center opacity-95'>
+					<motion.div className='grid gap-2 text-xl text-center opacity-95'
+						initial={{ opacity: 0 }}
+						animate={{ opacity: 1 }}
+						transition={{ duration: 1, delay: 1.5 }}
+					>
 						
 						<h1 className=' font-light text-4xl md:text-5xl '>
 							West Saint Paul
 						</h1>
 						<p className='font-bold tracking-wider'>1037 Dodd Rd</p>
 						{/* <p className=''>{`(651) 457-0777`}</p> */}
-					</div>
-					<div className='grid w-1/3   hover:opacity-90 hover:scale-110 transition duration-700 rounded-sm shadow-lg mx-auto text-center mt-2'>
+					</motion.div>
+					<motion.div 
+						initial={{ opacity: 0,  }}
+						animate={{ opacity: 1, }}
+						transition={{ duration: 2.1, delay: 1.5  }}
+
+					className='grid w-1/3   hover:opacity-90 hover:scale-110 transition duration-700 rounded-sm shadow-lg mx-auto text-center mt-2'>
 						<Link href={'/menu'}>
 							<a className=' text-xl  transition duration-700 bg-secondary p-4 font-tinos font-bold uppercase tracking-wider'>
 								Menu
 							</a>
 						</Link>
-					</div>
+					</motion.div>
 				</motion.div>
 			</div>
 		</div>
