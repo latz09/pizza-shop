93cb06779a3012eda6ba9ded706559e9d063172a
latz09 <85132328+latz09@users.noreply.github.com>
2023-05-01 14:47:45 -0500

    d

--- components/about/About.js
+++ components/about/About.js
@@ -4,7 +4,7 @@ const About = () => {
 			<div className='max-w-7xl text-light mx-auto px-2 md:px-0 text-2xl md:text-5xl tracking-widest h-[20vh] lg:h-[33vh] grid place-items-center bg-primary lg:rounded-lg shadow-lg border-2 border-secondary/50'>
 				<p className=' text-center'>
 					Proudly Serving the City of{' '}
-					<span className='text-light'> West Saint Paul</span> Since 19##
+					<span className='text-white font-semibold'> West Saint Paul</span> Since 19##
 				</p>
 			</div>
 		</>
