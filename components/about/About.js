fa221a8cd777c7dd8c61b1caadcd2ad91aac9785
latz09 <85132328+latz09@users.noreply.github.com>
2023-05-01 14:15:04 -0500

    d

--- components/about/About.js
+++ components/about/About.js
@@ -1,7 +1,7 @@
 const About = () => {
 	return (
 		<>
-			<div className='max-w-7xl mx-auto px-2 md:px-0 text-2xl md:text-5xl tracking-widest h-[33vh] grid place-items-center o'>
+			<div className='max-w-7xl mx-auto px-2 md:px-0 text-2xl md:text-5xl tracking-widest h-[20vh] lg:h-[33vh] grid place-items-center o'>
 				<p className=' text-center'>
 					Proudly Serving the City of{' '}
 					<span className='text-secondary'> West Saint Paul</span> Since 19##
