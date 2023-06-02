b6805a00f1dbc5a2abb914d5bd14a995f4f195fb
latz09 <85132328+latz09@users.noreply.github.com>
2023-06-02 12:47:29 -0500

    d

--- components/homepage/HoursLocation.js
+++ components/homepage/HoursLocation.js
@@ -1,14 +1,14 @@
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 
 const HoursLocation = () => {
+	const router = useRouter();
 	return (
 		<>
 			{' '}
 			<div className='grid gap-8   md:gap-5 items-center justify-items-center text-xl md:text-2xl py-8'>
 				<div className='grid justify-items-center gap-2'>
-					<span className=' tracking-wider text-primary '>
-						Monday - Friday
-					</span>
+					<span className=' tracking-wider text-primary '>Monday - Friday</span>
 					<span className='opacity-80'>4:30PM - 10PM</span>
 				</div>
 				<div className='grid justify-items-center gap-2'>
@@ -16,13 +16,15 @@ const HoursLocation = () => {
 					<span className='opacity-80'>4PM - 10PM</span>
 				</div>
 			</div>
-			<div className="text-center">
-				<Link href={'/menu'}>
-				<a className=' text-xl  transition duration-700 bg-secondary px-4 py-3 rounded shadow-lg  font-questrial uppercase tracking-wider text-light'>
-						Menu
-					</a>
-				</Link>
-			</div>
+			{router.pathname === '/' && (
+				<div className='text-center'>
+					<Link href={'/menu'}>
+						<a className=' text-xl  transition duration-700 bg-secondary px-4 py-3 rounded shadow-lg  font-questrial uppercase tracking-wider text-light'>
+							Menu
+						</a>
+					</Link>
+				</div>
+			)}
 		</>
 	);
 };
