12defbf89e70140e739df1d0e39ce962b705ee75
latz09 <85132328+latz09@users.noreply.github.com>
2023-06-02 12:07:10 -0500

    sticky menu

--- pages/menu.js
+++ pages/menu.js
@@ -11,49 +11,76 @@ const MenuPage = () => {
 	const [menuCategory, setMenuCategory] = useState(1);
 	const fadeIn = {
 		hidden: {
-		  opacity: 0,
+			opacity: 0,
 		},
 		visible: {
-		  opacity: 1,
-		  transition: {
-			duration: 0.5,
-		  },
+			opacity: 1,
+			transition: {
+				duration: 0.5,
+			},
 		},
-	  };
+	};
 
-	  
-	  
-	  return (
-		<div className="">
-		  <MenuHero menuCategory={menuCategory} setMenuCategory={setMenuCategory} />
-		  <div className="pb-24 mt-8">
-			<div className="">
-			  <AnimatePresence mode='wait'>
-				{menuCategory === 1 && (
-				  <motion.div key="byo" initial="hidden" animate="visible" exit="hidden" variants={fadeIn}>
-					<BuildYourOwn />
-				  </motion.div>
-				)}
-				{menuCategory === 2 && (
-				  <motion.div key="pf" initial="hidden" animate="visible" exit="hidden" variants={fadeIn}>
-					<PizzaFries />
-				  </motion.div>
-				)}
-				{menuCategory === 3 && (
-				  <motion.div key="as" initial="hidden" animate="visible" exit="hidden" variants={fadeIn}>
-					<AllSpecials />
-				  </motion.div>
-				)}
-				{menuCategory === 4 && (
-				  <motion.div key="sw" initial="hidden" animate="visible" exit="hidden" variants={fadeIn}>
-					<Sandwiches />
-				  </motion.div>
-				)}
-			  </AnimatePresence>
+	return (
+		<div className=''>
+			<div className="sticky top-[10vh] z-50 sm:relative sm:top-0">
+				<MenuHero
+					menuCategory={menuCategory}
+					setMenuCategory={setMenuCategory}
+				/>
 			</div>
-		  </div>
+			<div className='pb-24 mt-8'>
+				<div className=''>
+					<AnimatePresence mode='wait'>
+						{menuCategory === 1 && (
+							<motion.div
+								key='byo'
+								initial='hidden'
+								animate='visible'
+								exit='hidden'
+								variants={fadeIn}
+							>
+								<BuildYourOwn />
+							</motion.div>
+						)}
+						{menuCategory === 2 && (
+							<motion.div
+								key='pf'
+								initial='hidden'
+								animate='visible'
+								exit='hidden'
+								variants={fadeIn}
+							>
+								<PizzaFries />
+							</motion.div>
+						)}
+						{menuCategory === 3 && (
+							<motion.div
+								key='as'
+								initial='hidden'
+								animate='visible'
+								exit='hidden'
+								variants={fadeIn}
+							>
+								<AllSpecials />
+							</motion.div>
+						)}
+						{menuCategory === 4 && (
+							<motion.div
+								key='sw'
+								initial='hidden'
+								animate='visible'
+								exit='hidden'
+								variants={fadeIn}
+							>
+								<Sandwiches />
+							</motion.div>
+						)}
+					</AnimatePresence>
+				</div>
+			</div>
 		</div>
-	  );
+	);
 };
 
 export default MenuPage;
